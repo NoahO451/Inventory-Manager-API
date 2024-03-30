27de3fa900fd57c6636c9630d994ce2a9b5cfee5
Carlos43525 <carlos43525@gmail.com>
2024-03-30 18:06:53 -0400

    Added liquibase changelog and yaml files.

---
+++ db/changelog.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+
+--changeset your.name:1 labels:example-label context:example-context
+--comment: this is a test table that will be deleted soon.
+CREATE TABLE accounts (
+  user_id SERIAL PRIMARY KEY, 
+  username VARCHAR (50) UNIQUE NOT NULL, 
+  password VARCHAR (50) NOT NULL, 
+  email VARCHAR (255) UNIQUE NOT NULL, 
+  created_at TIMESTAMP NOT NULL, 
+  last_login TIMESTAMP
+);
