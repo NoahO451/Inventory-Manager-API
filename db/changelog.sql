03c4779998b9e8e96206c6db7687fa3bb2939e9c
Carlos43525 <carlos43525@gmail.com>
2024-03-31 01:31:17 -0400

    Updated changelog to include the user, inventory, and business tables, along with a join table. Also altered the changelog in such a way to require a checksum deletion.

--- db/changelog.sql
+++ db/changelog.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset your.name:1 labels:example-label context:example-context
+--changeset Carl:1 context:#8
 --comment: this is a test table that will be deleted soon.
 CREATE TABLE accounts (
   user_id SERIAL PRIMARY KEY, 
@@ -9,4 +9,56 @@ CREATE TABLE accounts (
   email VARCHAR (255) UNIQUE NOT NULL, 
   created_at TIMESTAMP NOT NULL, 
   last_login TIMESTAMP
+);
+
+--changeset Carl:2 context:#9
+--comment: this is a test table that will be deleted soon.
+DROP TABLE accounts;
+
+CREATE TABLE user_data (
+    user_id SERIAL PRIMARY KEY,
+    auth0_id TEXT UNIQUE NOT NULL,
+    username TEXT UNIQUE NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    role INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    last_login TIMESTAMP,
+    is_PremiumMember BOOLEAN NOT NULL,
+    is_Deleted BOOLEAN NOT NULL
+);
+
+CREATE TABLE inventory (
+    inventory_id BIGSERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+    sku TEXT,
+    cost NUMERIC(19,4),
+    serial_number TEXT,
+    purchased_date TIMESTAMP,
+    supplier TEXT,
+    brand TEXT,
+    model TEXT,
+    quantity INTEGER,
+    reorder_auantity INTEGER,
+    location TEXT,
+    expiration_date TIMESTAMP,
+    category INTEGER,
+    is_listed BOOLEAN,
+    is_lot BOOLEAN,
+    notes TEXT
+);
+
+CREATE TABLE user_business (
+    user_id INTEGER,
+    business_id INTEGER,
+    PRIMARY KEY (User_Id, Business_Id)
+);
+
+CREATE TABLE business (
+    business_id SERIAL PRIMARY KEY,
+    business_name TEXT,
+    business_type TEXT,
+    is_deleted BOOLEAN
 );
