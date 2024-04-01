a2e5dc745423933b3d6cc80645291a18faf65453
Carlos43525 <carlos43525@gmail.com>
2024-03-31 23:46:13 -0400

    Added DTO project to seperate dtos from api. Added new models for the database schema.

---
+++ BusinessManagement.API/Models/InventoryItem.cs
@@ -0,0 +1,26 @@
+﻿namespace App.Models
+{
+    public class InventoryItem
+    {
+        public long InventoryItemId { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? SKU { get; set; }
+        public decimal Cost { get; set; }
+        public string? SerialNumber { get; set; }
+        public DateTime? PurchasedDate { get; set; }
+        public string? Supplier { get; set; }
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public int Quantity { get; set; }
+        public int? ReorderQuantity { get; set; }
+        public string? Location { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public int? Category { get; set; }
+        public int? Packaging { get; set; }
+        public int? ItemWeight { get; set; } // weight is stored in kilos in the database
+        public bool IsListed { get; set; }
+        public bool IsLot { get; set; }
+        public string? Notes { get; set; }
+    }
+}
---
+++ BusinessManagement.API/Models/UserBusiness.cs
@@ -0,0 +1,8 @@
+﻿namespace App.Models
+{
+    public class UserBusiness
+    {
+        public int UserId { get; set; }
+        public int BusinessId { get; set; }
+    }
+}
