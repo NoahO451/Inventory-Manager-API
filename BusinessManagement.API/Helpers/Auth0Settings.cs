6cd51b35ac37dea671ad40361d5cb6b778510901
Carlos43525 <carlos43525@gmail.com>
2024-03-31 23:45:29 -0400

    Added dapper and related packages. Updated program.cs to use appsettings for auth0 instead of .env. Updated program.cs to grab dbsettings to establish db connection.

---
+++ BusinessManagement.API/Helpers/Auth0Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace App.Helpers
+{
+    public class Auth0Settings
+    {
+        public string? Port { get; set; }
+        public string? CLIENT_ORIGIN_URL { get; set; }
+        public string? AUTH0_AUDIENCE { get; set; }
+        public string? AUTH0_DOMAIN { get; set; }        
+    }
+}
---
+++ BusinessManagement.API/Helpers/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Extensions.Options;
+using Npgsql;
+
+namespace App.Helpers
+{
+    public class DataContext
+    {
+        private DbSettings _dbSettings;
+
+        public DataContext(IOptions<DbSettings> dbSettings)
+        {
+            _dbSettings = dbSettings.Value;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            var connectionString = $"Host={_dbSettings.Server}; Database={_dbSettings.Database}; Username={_dbSettings.UserId}; Password={_dbSettings.Password};";
+            return new NpgsqlConnection(connectionString);
+        }
+        public async Task Init()
+        {
+            // todo
+            //await _initDatabase();
+            //await _initTables();
+        }
+    }
+}
