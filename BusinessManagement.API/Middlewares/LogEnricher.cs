a331484af6c3a1738c48b7cbe2c5d36b28b61d6d
Carlos43525 <carlos43525@gmail.com>
2024-04-18 19:12:49 -0400

    Added more serilog config and custom enrichment.

---
+++ BusinessManagement.API/Middlewares/LogEnricher.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+using App.Extensions;
+
+namespace App.Middlewares
+{
+    public static class LogEnricher
+    {
+        /// <summary>
+        /// Enriches the HTTP request log with additional data via the Diagnostic Context
+        /// </summary>
+        /// <param name="diagnosticContext">The Serilog diagnostic context</param>
+        /// <param name="httpContext">The current HTTP Context</param>
+        public static void EnrichFromRequest(IDiagnosticContext diagnosticContext, HttpContext httpContext)
+        {
+            diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+            diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme.ToUpper());
+            diagnosticContext.Set("RemoteIpAddress", httpContext.Connection.RemoteIpAddress);
+            diagnosticContext.Set("Query", httpContext.Request.QueryString.Value);
+            //diagnosticContext.Set("UserAgent", httpContext.Request.Headers["User-Agent"].FirstOrDefault());
+        }
+    }
+}
