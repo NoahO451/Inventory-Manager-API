f8cd1f5db4ed81158a89db554963c189c57c694f
Decimal <DecimalCactus@gmail.com>
2024-07-12 01:11:49 -0400

    implement basic model for sales order and line item

---
+++ BusinessManagement.API/Models/SalesOrder.cs
@@ -0,0 +1,68 @@
+﻿using App.Models.ValueObjects;
+
+namespace App.Models
+{
+    public class SalesOrder
+    {
+        public SalesOrder() { }
+
+        public SalesOrder(Guid salesOrderUuid, string referenceNumber, Guid sOCustomerUuid, Guid businessUuid, 
+            DateTime createdAt, bool isTaxInclusive, SalesOrderDetail salesOrderDetail, Guid shippingAddressUuid, Guid billingAddressUuid, 
+            List<LineItem> lineItems)
+        {
+            if (salesOrderUuid == Guid.Empty)
+                throw new ArgumentNullException("Sales order uuid empty", nameof(salesOrderUuid));
+
+            if (sOCustomerUuid == Guid.Empty)
+                throw new ArgumentNullException("Customer uuid empty", nameof(sOCustomerUuid));
+
+            if (businessUuid == Guid.Empty)
+                throw new ArgumentNullException("Business uuid empty", nameof(businessUuid));
+
+            if (shippingAddressUuid == Guid.Empty)
+                throw new ArgumentNullException("Shipping address uuid empty", nameof(shippingAddressUuid));
+
+            if (billingAddressUuid == Guid.Empty)
+                throw new ArgumentNullException("Billing address uuid empty", nameof(billingAddressUuid));
+
+            if (lineItems == null)
+                throw new ArgumentNullException("Line items are null", nameof(lineItems));
+
+            if (createdAt < DateTime.Now)
+                throw new ArgumentException("Created at time cannot be in the past", nameof(createdAt));
+
+
+            SalesOrderUuid = salesOrderUuid;
+            ReferenceNumber = referenceNumber;
+            SOCustomerUuid = sOCustomerUuid;
+            BusinessUuid = businessUuid;
+            CreatedAt = createdAt;
+            IsTaxInclusive = isTaxInclusive;
+            SalesOrderDetail = salesOrderDetail;
+            ShippingAddressUuid = shippingAddressUuid;
+            BillingAddressUuid = billingAddressUuid;
+            LineItems = lineItems;
+        }
+
+        public void SetSalesOrderDetail(SalesOrderDetail salesOrderDetail)
+        {
+            SalesOrderDetail = salesOrderDetail;
+        }
+
+        public void SetLineItem(List<LineItem> lineItems)
+        {
+            LineItems = lineItems;
+        }
+
+        public Guid SalesOrderUuid { get; private set; }
+        public string ReferenceNumber { get; private set; }
+        public Guid SOCustomerUuid { get; private set; }
+        public Guid BusinessUuid { get; private set; } //might not need this
+        public DateTime CreatedAt { get; private set; }
+        public bool IsTaxInclusive { get; private set; }
+        public SalesOrderDetail SalesOrderDetail { get; private set; }
+        public Guid ShippingAddressUuid { get; private set; }
+        public Guid BillingAddressUuid { get; private set; }
+        public List<LineItem> LineItems { get; private set; }
+    }
+}
---
+++ BusinessManagement.API/Models/ValueObjects/LineItem.cs
@@ -0,0 +1,32 @@
+﻿namespace App.Models.ValueObjects
+{
+    public record LineItem
+    {
+        public LineItem() { }
+
+        public LineItem(Guid lineItemUuid, Guid inventoryItemUuid, int orderNumber, string name, int cost, 
+            string? description, int quantity)
+        {
+            if (lineItemUuid == Guid.Empty)
+                throw new ArgumentNullException("Line item uuid empty", nameof(lineItemUuid));
+
+            if (inventoryItemUuid == Guid.Empty)
+                throw new ArgumentNullException("Inventory item uuid empty", nameof(inventoryItemUuid));
+
+            LineItemUuid = lineItemUuid;
+            InventoryItemUuid = inventoryItemUuid;
+            OrderNumber = orderNumber;
+            Name = name;
+            Cost = cost;
+            Description = description;
+            Quantity = quantity;
+        }
+        public Guid LineItemUuid { get; private set; }
+        public Guid InventoryItemUuid { get; private set; }
+        public int OrderNumber { get; private set; }
+        public string Name { get; private set; }
+        public int Cost { get; private set; } //stored in pennies 
+        public string? Description { get; private set; }
+        public int Quantity { get; private set; }
+    }
+}
